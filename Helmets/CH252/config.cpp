474f5e57c263436fc896cc0574ea5c581303985d
GeorgeAlfonso <280772butakov@gmail.com>
2023-10-19 23:56:58 +1000

    Характеристики шлема, как у OPTRE

--- Helmets/CH252/config.cpp
+++ Helmets/CH252/config.cpp
@@ -113,29 +113,6 @@ class CfgWeapons
 		picture = "\OPTRE_UNSC_Units\Army\icons\Army_Helmet_wdl.paa";
 		hiddenSelections[] = {"camo", "camo2", "camo3", "camo4", "H_Collar", "H_Neck", "H_UNSCVacLower", "H_UNSCVacVisor", "H_VacCollar", "H_Ghillie"};
 		hiddenSelectionsTextures[] = {"division7_gear\Helmets\CH252\_textures\Div7_LI_CH252.paa", "optre_unsc_units\army\data\helmet_visor_ca.paa", "optre_unsc_units\army\data\ghillie_woodland_co.paa", "optre_unsc_units\army\data\soft_packs_co.paa"};
-		class ItemInfo : HeadgearItem
-		{
-			uniformModel = "\OPTRE_UNSC_Units\Army\helmet.p3d";
-			mass = 40;
-			modelSides[] = {6};
-			passThrough = 0.1;
-			hiddenSelections[] = {"camo", "camo2", "camo3", "camo4", "H_Collar", "H_Neck", "H_UNSCVacLower", "H_UNSCVacVisor", "H_VacCollar", "H_Ghillie"};
-			class HitpointsProtectionInfo
-			{
-				class Head
-				{
-					hitpointName = "HitHead";
-					armor = 30;
-					passThrough = 0.1;
-				};
-				class Face
-				{
-					hitpointName = "HitFace";
-					armor = 30;
-					passThrough = 0.1;
-				};
-			};
-		};
 	};
 
 	class DIV7_ECH252_Helmet_Base : OPTRE_UNSC_CH252_Helmet2_Vacuum_WDL
